8043f5e24d8aff7fbba7bf7c4dd87178fb059b78
Jhonbeltran <jbeltranleon@gmail.com>
2017-07-19 12:35:30 -0500

    Agrego ejemplo de web scraping con python

--- web_scraping.py
+++ web_scraping.py
@@ -1,1 +1,16 @@
+import requests
 import urllib.request
+from bs4 import BeautifulSoup
+
+def main():
+	for i in range(1, 6):
+		response = requests.get('https://xkcd.com/{}'.format(i))
+		soup = BeautifulSoup(response.content, 'html.parser')
+		image_container = soup.find(id='comic')
+		image_url = image_container.find('img')['src']
+		image_name = image_url.split('/')[-1]
+		print('Descargando la imagen {}'.format(image_name))
+		urllib.request.urlretrieve('https:{}'.format(image_url), image_name)
+
+if __name__ == '__main__':
+	main()
